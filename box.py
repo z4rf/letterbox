902a4387d649b54c94403045592355c7796536e1
z4rf <raminzarf@gmail.com>
2024-11-09 15:36:38 -0500

    additional skeleton and some initial code

--- box.py
+++ box.py
@@ -1,12 +1,19 @@
 # initial thoughts
 
+
+# dictionary file
+#     iterate through and create a 26 node tree
+
+# ingest puzzle sides
+puzzle = input("Enter puzzle letters, begin from top left corner: ")
+sides = [puzzle[0:3], puzzle[3:6], puzzle[6:9], puzzle[9:12]]
+
+# follow game logic, recursively test each path
+for side in puzzle: 
+    for letter in side:
+        discoverWords() 
+
 '''
-dictionary file
-    iterate through and create a 26 node tree
-
-ingest puzzle sides
-
-follow game logic, recursively test each path
 every new letter that's considered
     check if path exists for it in dictionary tree 
 if a word is discovered, add it to a word bank
