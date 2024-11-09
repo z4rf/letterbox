e83a339818e48b066295210bb14185cc851ea530
Ramin Zarfeshan <noodle@n00dl3.mynetworksettings.com>
2024-11-09 15:02:23 -0500

    adding initial files

---
+++ box.py
@@ -0,0 +1,19 @@
+# initial thoughts
+
+'''
+dictionary file
+    iterate through and create a 26 node tree
+
+ingest puzzle sides
+
+follow game logic, recursively test each path
+every new letter that's considered
+    check if path exists for it in dictionary tree 
+if a word is discovered, add it to a word bank
+
+with the word bank
+    
+
+
+
+'''
