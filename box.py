4b7747071334184a5bfa603f69ddc92cfd318c8d
z4rf <raminzarf@gmail.com>
2024-11-09 15:19:14 -0500

    testing new username

--- box.py
+++ box.py
@@ -11,8 +11,13 @@ every new letter that's considered
     check if path exists for it in dictionary tree 
 if a word is discovered, add it to a word bank
 
+make array of answers
 with the word bank
-    
+    pop first word from wordlist, add to word array
+    grab last letter of first word
+    iterate through rest of list
+no no no no, it's gonna need to be recursive
+this might take some more thought than expected
 
 
 
