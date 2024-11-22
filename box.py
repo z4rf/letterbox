c6269593d15fe18af2181ef4410e28566011e801
z4rf <raminzarf@gmail.com>
2024-11-21 22:47:51 -0500

    ope, reached max recursion depth (yikes)

--- box.py
+++ box.py
@@ -1,27 +1,27 @@
-class node:
-    
-    next = [None] * 26
+import pdb
 
+class node:
+    next = [None] * 26
     leaf = False
-    valid = False
 
 list = node()
 puzzle = ""
 viableWords = []
 
 def main():
-
+    print("running main...")
     # dictionary file
     #     iterate through and create a 26 node tree
-    list.buildDict("words_alpha.txt")
+    buildDict("words_alpha.txt")
 
     # ingest puzzle sides
     puzzle = input("Enter puzzle letters, begin from top left corner: ")
 
     # follow game logic, recursively test each path
     for i in range (0, 12):
-        discoverWord(puzzle[i], i)
+        discoverWord(puzzle[i], i, puzzle)
 
+    print(f"list of viable words: {viableWords}")
     '''
     every new letter that's considered
         check if path exists for it in dictionary tree 
@@ -37,18 +37,20 @@ def main():
 
     '''
 
-def discoverWord(word, position):
-    if not list.letterExists(word):
+def discoverWord(word, position, puzzle):
+    if not letterExists(word):
+        print(f"not a real word: {word}")
         return
-    if (list.wordIsLeaf(word)):
-        viableWords.add(word)
+    if (wordIsLeaf(word)):
+        viableWords.append(word)
 
     # snap position to next side
     position = ((position + 3) // 3) * 3
 
     # try 9 valid choices for next letter
-    for i in range(position, (position + 9) % 12):
-        discoverWord(word + puzzle[i], i)
+    for i in range(position, position + 9):
+        # pdb.set_trace()
+        discoverWord(word + puzzle[i%12], i%12, puzzle)
 
 # word tree:
 # 26-tree
@@ -64,18 +66,23 @@ def discoverWord(word, position):
 
 
 def buildDict(filename):
+    counter = 0
     with open(filename) as file:
         for word in file:
-            insert(word)
+            if counter % 10000 == 0: print(f"inserting {counter}th word {word.rstrip()} into trie...")
+            insert(word.rstrip())
+            counter += 1
 
 def insert(word):
     current = list 
     for letter in word:
+        # print(f"current letter: {letter}")
         index = ord(letter) - ord('a')
+        # print(f"index: {index}")
         if not current.next[index]:
             current.next[index] = node()
             current = current.next[index]
-    current.isLeaf = True
+    current.leaf = True
 
 def wordIsLeaf(word):
     # traverse down word tree and check if where you land is a leaf
@@ -85,7 +92,7 @@ def wordIsLeaf(word):
         if not current.next[index]:
             return False
         current = current.next[index]
-    return current.isLeaf
+    return current.leaf
 
 def letterExists(wor):
     # similar but not the same.  if node you land on is the end of a valid word, return true.  does not indicate anything about leaf status though
@@ -101,3 +108,5 @@ def letterExists(wor):
     return current != None
 
 # RAMIN: tweak discoverWord to deal with the fact that leafs can have children in this implementation
+
+main()
